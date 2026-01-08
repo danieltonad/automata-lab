58abcde68af6360546a9815ea08bf29456b2f971
danieltonad <soladan2000@gmail.com>
2026-01-08 09:43:36 +0100

    Remove unused 'psutil' dependency from requirements files

--- tiktok-page/tiktok_page.py
+++ tiktok-page/tiktok_page.py
@@ -1,12 +1,9 @@
-import asyncio, argparse, sys, math, time
-import psutil, re
+import asyncio, argparse, sys, math, time, re
 from pathlib import Path
 from playwright.async_api import async_playwright
 from playwright_stealth import Stealth
 from dataclasses import dataclass
 from typing import List, Set, Tuple
-
-from tiktok.tiktok import TiktokPageMetadata
 
 
 @dataclass
@@ -20,7 +17,7 @@ class TiktokPageMetadata:
     link: str
     videos: List[str] = None
     playlists: List[str] = None
-    repos: List[str] = None
+    reposts: List[str] = None
 
 
 class Colors:
@@ -55,32 +52,69 @@ def time_taken(start: float, stop: float) -> str:
 def is_tiktok_url(url: str) -> bool:
     return "tiktok.com/" in url
 
-def get_author_from_url(url: str) -> str:
-    match = re.search(r'tiktok\.com/@([^/]+)/', url)
-    return match.group(1) if match else ""
-
 
 def save_tiktok_metadata_json(metadatas: List[TiktokPageMetadata], filepath: Path) -> None:
     import json
     with open(filepath, 'w', encoding='utf-8') as output_file:
         json.dump([s.__dict__ for s in metadatas], output_file, ensure_ascii=False, indent=4)
 
-async def fetch_tiktok_page_metadata(url: str, page, retry: int = 0) -> TiktokPageMetadata:
-    await page.goto(url, timeout=60000)
-    comment_count = await page.locator('strong[data-e2e="comment-count"]').first.inner_text()
-    likes = await page.locator('strong[data-e2e="like-count"]').first.inner_text()
-    bookmarks = await page.locator('strong[data-e2e="undefined-count"]').first.inner_text()
-    shares = await page.locator('strong[data-e2e="share-count"]').first.inner_text()
-    description = await page.locator('div[data-e2e="video-desc"]').first.inner_text()
+
+async def pull_info_from_page(page, url: str) -> TiktokPageMetadata:
+    await page.goto(url)
+    
+    author = await page.locator('h1[data-e2e="user-title"]').inner_text()
+    name = await page.locator('h2[data-e2e="user-subtitle"]').inner_text()
+    following = await page.locator('strong[data-e2e="following-count"]').inner_text()
+    followers = await page.locator('strong[data-e2e="followers-count"]').inner_text()
+    likes = await page.locator('strong[data-e2e="likes-count"]').inner_text()
+    bio = await page.locator('h2[data-e2e="user-bio"]').inner_text()
+    # link = await page.locator('span[class="css-1u5503g-5e6d46e3--SpanLink e1dvixx72"]').inner_text()
+
+    await page.click('body')
+    await page.keyboard.press("ArrowDown")
+    loading = await page.locator('svg circle[stroke="#3AF2FF"]').is_visible()
+    await page.mouse.wheel(0, 5000)
+    await asyncio.sleep(5)
+    print("Loading indicator visible:", loading)
+    loading = await page.locator('svg circle[stroke="#3AF2FF"]').is_visible()
+    print("Loading indicator visible:", loading)
+    await page.keyboard.press("ArrowDown")
+
+    return TiktokPageMetadata(
+        name=name,
+        author=author,
+        following=following,
+        followers=followers,
+        likes=likes,
+        bio=bio,
+        link=None
+    )
 
 
+
+async def fetch_tiktok_page_metadata(url: str) -> None:
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context()
+        await Stealth().apply_stealth_async(context)
+        page = await context.new_page()
+        metadata = await pull_info_from_page(page, url)
+        print(metadata)
+        await browser.close()
+
+
+def normalize_page_input(page_input: str) -> str:
+    if is_tiktok_url(page_input):
+        return page_input
+    else:
+        return f"https://www.tiktok.com/@{page_input}"
 
 async def main():
     if len(sys.argv) < 2:
         print("Usage: python tiktok_page.py <page_name_or_link_or_id>")
         sys.exit(1)
     page_input = sys.argv[1]
-    await grab_channel_info(normalize_page_input(page_input))
+    await fetch_tiktok_page_metadata(normalize_page_input(page_input))
 
 
 if __name__ == "__main__":
